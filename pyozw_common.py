4f4935814ee61ab6eb175399ec4566699d9229e2
Kevin Schlosser <drschlosser@hotmail.com>
2019-01-01 03:17:56 -0700

    test commit

--- pyozw_common.py
+++ pyozw_common.py
@@ -17,6 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with python-openzwave. If not, see http://www.gnu.org/licenses.
 """
+
 from __future__ import print_function
 import os
 import sys
